84d153c92b51ef1663a202d5b426501a45757952
Labib2003 <labib.salimi30@gmail.com>
2024-11-08 21:49:21 +0600

    feat: taking port from env

--- app/index.js
+++ app/index.js
@@ -58,6 +58,7 @@ ws.on("connection", (connection) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log("server running on port 5000");
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
